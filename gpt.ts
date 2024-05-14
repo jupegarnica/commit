f5caf3fbcae19303708c249eaf32b2cca3865ab6
Jupegarnica <juan@geekshubs.com>
2024-05-14 08:17:06 +0200

    refactor: modularize GPT function and update OpenAI import version

--- gpt.ts
+++ gpt.ts
@@ -1,56 +1,79 @@
-import OpenAI from 'npm:openai';
-const MAX_TOKENS = 6_000;
-
-const isLlama3 = Deno.args.includes('--llama3');
-
-const model = isLlama3 ? 'llama3' : "gpt-4-turbo-preview";
-const baseURL = isLlama3 ? 'http://localhost:11434/v1' : undefined;
-const openai = new OpenAI({
-    apiKey: Deno.env.get('OCO_OPENAI_API_KEY'),
-    baseURL,
-});
-
-let content = Deno.args.join(' ') + '\n';
-if (!Deno.stdin.isTerminal()) {
-    const decoder = new TextDecoder();
-    for await (const chunk of Deno.stdin.readable) {
-        content += decoder.decode(chunk);
-    }
-}
 
 
-const words = content.split(' ').length;
-// console.warn({ words });
-if (words > MAX_TOKENS) {
-    console.warn({ content, words });
-    console.error(`Input is too long: ${words} words`);
-    Deno.exit(1);
-}
+
+import OpenAI from 'npm:openai@4.38.3';
 
 
-const chatCompletion = await openai.chat.completions.create({
+export async function gpt({
     model,
-    // model: 'llama3',
-    messages: [
-        {
-            role: "system",
-            content: "You are a expert programmer. You are helping a user to write a code snippet. You should use the best practices and idiomatic code to write a code snippet for the given problem. If the code snippet is empty return only zero characters."
-        },
-        {
-            role: "user",
-            content
-        }
-    ],
-    temperature: 0,
-    stream: false,
-});
-const response = chatCompletion.choices[0].message.content;
+    apiKey,
+    baseURL,
+    content,
+    systemContent,
+}: {
+    model: string;
+    apiKey: string;
+    content: string;
+    systemContent: string;
+    baseURL?: string;
 
-if (!response) {
-    console.error('No response');
-    Deno.exit(1);
+}): Promise<string> {
+
+
+
+    const openai = new OpenAI({
+        apiKey,
+        baseURL,
+    });
+
+
+
+    const chatCompletion = await openai.chat.completions.create({
+        model,
+        messages: [
+            {
+                role: "system",
+                content: systemContent
+            },
+            {
+                role: "user",
+                content: content
+            }
+        ],
+        temperature: 0,
+        stream: false,
+    });
+
+    return chatCompletion.choices[0].message.content || "";
+
+
 }
-// const model = chatCompletion.model;
-//console.error(model);
 
-console.log(response);
+
+if (import.meta.main) {
+    let content = Deno.args.join(' ') + '\n';
+    if (!Deno.stdin.isTerminal()) {
+        const decoder = new TextDecoder();
+        for await (const chunk of Deno.stdin.readable) {
+            content += decoder.decode(chunk);
+        }
+    }
+
+    const MAX_TOKENS = 6_000;
+    const words = content.split(' ').length;
+    // console.warn({ words });
+    if (words > MAX_TOKENS) {
+        console.warn({ content, words });
+        console.error(`Input is too long: ${words} words`);
+        Deno.exit(1);
+    }
+    const response = await gpt({
+        model: "gpt-4o",
+        apiKey: Deno.env.get('OCO_OPENAI_API_KEY')!,
+        baseURL: undefined,
+        content: content,
+        systemContent: "You are a expert programmer. You are helping a user to write a code snippet. You should use the best practices and idiomatic code to write a code snippet for the given problem. If the code snippet is empty return only zero characters."
+    });
+
+    console.log(response);
+}
