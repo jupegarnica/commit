e9665c2e563ccb13c98526d1accd208bf4e0485e
Juan Garnica <juan@harbiz.io>
2024-10-27 10:46:42 +0100

    style: add newline for improved readability in commit.ts

--- commit.ts
+++ commit.ts
@@ -1,44 +1,48 @@
 import $ from "jsr:@david/dax@0.40.1";
-import { parseArgs } from "jsr:@std/cli@0.223.0"
+import { parseArgs } from "jsr:@std/cli@0.223.0";
 import { gpt } from "./gpt.ts";
 
-
 async function dax(strings: TemplateStringsArray, ...values: unknown[]) {
-
-    try {
-        return await $.raw(strings, ...values.map(String))
-    } catch (_error) {
-        // console.error(error.message);
-        Deno.exit(1);
-
-    }
+  try {
+    return await $.raw(strings, ...values.map(String));
+  } catch (_error) {
+    // console.error(error.message);
+    Deno.exit(1);
+  }
 }
 
 async function daxSilent(strings: TemplateStringsArray, ...values: unknown[]) {
-
-    try {
-        return await $.raw(strings, ...values.map(String)).text();
-    } catch (error) {
-        console.error(error);
-        Deno.exit(1);
-    }
+  try {
+    return await $.raw(strings, ...values.map(String)).text();
+  } catch (error) {
+    console.error(error);
+    Deno.exit(1);
+  }
 }
 
-
-
 export async function commit(): Promise<void> {
-    const args = parseArgs(Deno.args, {
-        boolean: ['add', 'push', 'ollama', 'debug', 'config', 'skip-edit', 'no-commit', 'help'],
-        string: ['api-key', 'model', 'base-URL', 'max-words', 'commits-to-learn'],
-    });
-    const MAX_TOKENS = Number(args['max-words']) || 6_000;
-    const debug = args.debug || false;
-    let model = args.model || 'gpt-4o';  // || 'gpt-4o-mini';
-    let baseURL: string | undefined = undefined;
-    let apiKey = args['api-key'] ||  localStorage.getItem('OPENAI_API_KEY') || Deno.env.get('OPENAI_API_KEY');
+  const args = parseArgs(Deno.args, {
+    boolean: [
+      "add",
+      "push",
+      "ollama",
+      "debug",
+      "config",
+      "skip-edit",
+      "no-commit",
+      "help",
+    ],
+    string: ["api-key", "model", "base-URL", "max-words", "commits-to-learn"],
+  });
+  const MAX_TOKENS = Number(args["max-words"]) || 6_000;
+  const debug = args.debug || false;
+  let model = args.model || "gpt-4o"; // || 'gpt-4o-mini';
+  let baseURL: string | undefined = undefined;
+  let apiKey = args["api-key"] || localStorage.getItem("OPENAI_API_KEY") ||
+    Deno.env.get("OPENAI_API_KEY");
 
-    if (args.help) {
-        console.info(`Usage: commit [options]
+  if (args.help) {
+    console.info(`Usage: commit [options]
 
 --add: Runs git add . before creating the commit message.
 --push: Runs git push after the commit creation.
@@ -57,106 +61,108 @@ export async function commit(): Promise<void> {
 --version: Prints the version number.
 
        `);
-        return;
+    return;
+  }
+
+  if (args.version) {
+    let version: string | undefined;
+    if (import.meta.url.startsWith("http")) {
+      const response = await fetch(new URL("./deno.json", import.meta.url));
+      const json = await response.json();
+      version = json.version;
+    } else {
+      version = JSON.parse(
+        await Deno.readTextFile(new URL("./deno.json", import.meta.url)),
+      ).version;
     }
+    debug && console.debug("import.meta.url", import.meta.url);
+    console.info(version);
+    return;
+  }
 
-    if (args.version) {
-        let version: string | undefined;
-        if (import.meta.url.startsWith('http')) {
-            const response = await fetch(new URL('./deno.json', import.meta.url));
-            const json = await response.json();
-            version = json.version;
-        } else {
-            version = JSON.parse(await Deno.readTextFile(new URL('./deno.json', import.meta.url))).version;
-        }
-        debug && console.debug('import.meta.url', import.meta.url );
-        console.info(version);
-        return;
+  if (!apiKey || args.config) {
+    apiKey = await $.prompt("Enter OpenAI API Key");
+    localStorage.setItem("OPENAI_API_KEY", apiKey);
+    if (args.config) {
+      console.info("API Key saved");
+      return;
     }
+  }
 
-    if (!apiKey || args.config) {
-        apiKey = await $.prompt('Enter OpenAI API Key');
-        localStorage.setItem('OPENAI_API_KEY', apiKey);
-        if (args.config) {
-            console.info('API Key saved');
-            return;
-        }
-    }
+  if (args.add) {
+    await $`git add .`;
+  }
 
-    if (args.add) {
-        await $`git add .`;
-    }
+  if (args.ollama) {
+    model = args.model || "llama3";
+    baseURL = args["base-URL"] || "http://localhost:11434/v1";
+  }
+  debug && console.debug({ args, model, baseURL });
+  const diff =
+    await daxSilent`git diff --unified=5 --staged -- . ':(exclude)*.lock'`;
+  debug && console.debug({ diff });
 
-    if (args.ollama) {
-        model = args.model || 'llama3';
-        baseURL = args['base-URL'] || 'http://localhost:11434/v1';
+  if (!diff) {
+    console.error(
+      "No staged changes to commit. \nUse --add flag to add all changes to commit, or use git add for specific files.",
+    );
+    return Deno.exit(1);
+  }
 
-    }
-    debug && console.debug({ args,  model, baseURL });
-    const diff = await daxSilent`git diff --unified=5 --staged -- . ':(exclude)*.lock'`
-    debug && console.debug({ diff });
+  const words = diff.split(" ").length;
+  debug && console.debug({ words });
+  if (words > MAX_TOKENS) {
+    console.error(`Input is too long: ${words} words`);
+    Deno.exit(1);
+  }
+  const commitsToLearn = Number(args["commits-to-learn"]) || 10;
+  if (isNaN(commitsToLearn)) {
+    console.error(`Invalid commitsToLearn: ${commitsToLearn}`);
+    Deno.exit(1);
+  }
+  let commits = "";
+  if (commitsToLearn > 0) {
+    commits = await daxSilent`git log --oneline -n ${commitsToLearn}`;
+    debug && console.debug({ commits });
+  }
 
-    if (!diff) {
-        console.error('No staged changes to commit. \nUse --add flag to add all changes to commit, or use git add for specific files.');
-        return Deno.exit(1);
-    }
+  let systemContent =
+    "You are a expert in git diffs. You are helping a user to create a commit message for a git diff. You should use conventional commit notation to create a commit message for this git diff. Do not use any markdown markup, only text. If the git diff is empty return only zero characters. Only include the commit message, do not include anything else, just the commit message without any quotes or backticks.";
+  if (commits) {
+    systemContent +=
+      `\nYou should follow the commit style of this commits:\n${commits}`;
+  }
 
+  let commitMessage: string = await gpt({
+    model,
+    apiKey,
+    baseURL,
+    content: diff,
+    systemContent,
+  });
+  commitMessage = commitMessage?.trim().replace(/(^['"`]|$['"`])/, "").replace(
+    /`/g,
+    "'",
+  );
+  debug && console.debug({ commitMessage });
+  if (!commitMessage) {
+    console.error("No commitMessage");
+    Deno.exit(1);
+  }
 
+  if (args["no-commit"]) {
+    console.info(commitMessage);
+    return;
+  }
+  const edit = args["skip-edit"] ? "" : " --edit";
+  const amend = args.amend ? " --amend" : "";
+  await dax`git commit ${amend} ${edit} -m "${commitMessage}"`;
 
-
-    const words = diff.split(' ').length;
-    debug && console.debug({ words });
-    if (words > MAX_TOKENS) {
-        console.error(`Input is too long: ${words} words`);
-        Deno.exit(1);
-    }
-    const commitsToLearn = Number(args['commits-to-learn']) || 10;
-    if (isNaN(commitsToLearn)) {
-        console.error(`Invalid commitsToLearn: ${commitsToLearn}`);
-        Deno.exit(1);
-    }
-    let commits = '';
-    if (commitsToLearn > 0) {
-        commits = await daxSilent`git log --oneline -n ${commitsToLearn}`;
-        debug && console.debug({commits});
-    }
-
-
-    let systemContent = "You are a expert in git diffs. You are helping a user to create a commit message for a git diff. You should use conventional commit notation to create a commit message for this git diff. Do not use any markdown markup, only text. If the git diff is empty return only zero characters. Only include the commit message, do not include anything else, just the commit message without any quotes or backticks."
-    if (commits) {
-        systemContent += `\nYou should follow the commit style of this commits:\n${commits}`;
-    }
-    let commitMessage: string = await gpt({
-        model,
-        apiKey,
-        baseURL,
-        content: diff,
-        systemContent,
-    })
-    commitMessage = commitMessage?.trim().replace(/(^['"`]|$['"`])/, "").replace(/`/g, "'");
-    debug && console.debug({ commitMessage });
-    if (!commitMessage) {
-        console.error('No commitMessage');
-        Deno.exit(1);
-    }
-
-    if (args['no-commit']) {
-        console.info(commitMessage);
-        return;
-
-    }
-    const edit = args['skip-edit'] ? '' : ' --edit';
-    const amend = args.amend ? ' --amend' : '';
-    await dax`git commit ${amend} ${edit} -m "${commitMessage}"`;
-
-    if (args.push) {
-        await $`git push`;
-    }
-
-
+  if (args.push) {
+    await $`git push`;
+  }
 }
 
-
 if (import.meta.main) {
-    await commit();
+  await commit();
 }
--- gpt.ts
+++ gpt.ts
@@ -1,79 +1,67 @@
-
-
-
-import OpenAI from 'npm:openai@4.38.3';
-
+import OpenAI from "npm:openai@4.38.3";
 
 export async function gpt({
-    model,
+  model,
+  apiKey,
+  baseURL,
+  content,
+  systemContent,
+}: {
+  model: string;
+  apiKey: string;
+  content: string;
+  systemContent: string;
+  baseURL?: string;
+}): Promise<string> {
+  const openai = new OpenAI({
     apiKey,
     baseURL,
-    content,
-    systemContent,
-}: {
-    model: string;
-    apiKey: string;
-    content: string;
-    systemContent: string;
-    baseURL?: string;
+  });
 
-}): Promise<string> {
+  const chatCompletion = await openai.chat.completions.create({
+    model,
+    messages: [
+      {
+        role: "system",
+        content: systemContent,
+      },
+      {
+        role: "user",
+        content: content,
+      },
+    ],
+    temperature: 0,
+    stream: false,
+  });
 
-
-
-    const openai = new OpenAI({
-        apiKey,
-        baseURL,
-    });
-
-
-
-    const chatCompletion = await openai.chat.completions.create({
-        model,
-        messages: [
-            {
-                role: "system",
-                content: systemContent
-            },
-            {
-                role: "user",
-                content: content
-            }
-        ],
-        temperature: 0,
-        stream: false,
-    });
-
-    return chatCompletion.choices[0].message.content || "";
-
-
+  return chatCompletion.choices[0].message.content || "";
 }
 
-
 if (import.meta.main) {
-    let content = Deno.args.join(' ') + '\n';
-    if (!Deno.stdin.isTerminal()) {
-        const decoder = new TextDecoder();
-        for await (const chunk of Deno.stdin.readable) {
-            content += decoder.decode(chunk);
-        }
+  let content = Deno.args.join(" ") + "\n";
+  if (!Deno.stdin.isTerminal()) {
+    const decoder = new TextDecoder();
+    for await (const chunk of Deno.stdin.readable) {
+      content += decoder.decode(chunk);
     }
+  }
 
-    const MAX_TOKENS = 6_000;
-    const words = content.split(' ').length;
-    // console.warn({ words });
-    if (words > MAX_TOKENS) {
-        console.warn({ content, words });
-        console.error(`Input is too long: ${words} words`);
-        Deno.exit(1);
-    }
-    const response = await gpt({
-        model: "gpt-4o-mini",
-        apiKey: Deno.env.get('OCO_OPENAI_API_KEY')!,
-        baseURL: undefined,
-        content: content,
-        systemContent: "You are a expert programmer. You are helping a user to write a code snippet. You should use the best practices and idiomatic code to write a code snippet for the given problem. If the code snippet is empty return only zero characters."
-    });
+  const MAX_TOKENS = 6_000;
+  const words = content.split(" ").length;
+  // console.warn({ words });
+  if (words > MAX_TOKENS) {
+    console.warn({ content, words });
+    console.error(`Input is too long: ${words} words`);
+    Deno.exit(1);
+  }
+  const response = await gpt({
+    model: "gpt-4o-mini",
+    apiKey: Deno.env.get("OCO_OPENAI_API_KEY")!,
+    baseURL: undefined,
+    content: content,
+    systemContent:
+      "You are a expert programmer. You are helping a user to write a code snippet. You should use the best practices and idiomatic code to write a code snippet for the given problem. If the code snippet is empty return only zero characters.",
+  });
 
-    console.log(response);
+  console.log(response);
 }
